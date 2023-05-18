2c8b78772fcba75b469aea50afa6323510c74881
vongomben <davide.gomba@gmail.com>
2023-05-18 22:29:21 +0200

    updatiing behaviours in the night

--- Serial-servo/Serial-servo.ino
+++ Serial-servo/Serial-servo.ino
@@ -3,8 +3,8 @@
 Servo myservo;  // create servo object to control a servo
 // 16 servo objects can be created on the ESP32
 
-int pos = 0;    // variable to store the servo position
-// Recommended PWM GPIO pins on the ESP32 include 2,4,12-19,21-23,25-27,32-33 
+int pos = 0;  // variable to store the servo position
+// Recommended PWM GPIO pins on the ESP32 include 2,4,12-19,21-23,25-27,32-33
 // Possible PWM GPIO pins on the ESP32-S2: 0(used by on-board button),1-17,18(used by on-board LED),19-21,26,33-42
 // Possible PWM GPIO pins on the ESP32-S3: 0(used by on-board button),1-21,35-45,47,48(used by on-board LED)
 // Possible PWM GPIO pins on the ESP32-C3: 0(used by on-board button),1-7,8(used by on-board LED),9-10,18-21
@@ -25,16 +25,16 @@ void setup() {
   Serial.begin(9600);
   // initialize the LED pin as an output:
   pinMode(ledPin, OUTPUT);
- 	// Allow allocation of all timers
-	ESP32PWM::allocateTimer(0);
-	ESP32PWM::allocateTimer(1);
-	ESP32PWM::allocateTimer(2);
-	ESP32PWM::allocateTimer(3);
-	myservo.setPeriodHertz(50);    // standard 50 hz servo
-	myservo.attach(servoPin, 1000, 2000); // attaches the servo on pin 18 to the servo object
-	// using default min/max of 1000us and 2000us
-	// different servos may require different min/max settings
-	// for an accurate 0 to 180 sweep
+  // Allow allocation of all timers
+  ESP32PWM::allocateTimer(0);
+  ESP32PWM::allocateTimer(1);
+  ESP32PWM::allocateTimer(2);
+  ESP32PWM::allocateTimer(3);
+  myservo.setPeriodHertz(50);            // standard 50 hz servo
+  myservo.attach(servoPin, 1000, 2000);  // attaches the servo on pin 18 to the servo object
+                                         // using default min/max of 1000us and 2000us
+                                         // different servos may require different min/max settings
+                                         // for an accurate 0 to 180 sweep
 }
 
 void loop() {
@@ -63,6 +63,31 @@ void loop() {
       delay(1000);
       myservo.write(100);
       delay(1500);
+      myservo.write(90);
+    }
+
+    if (incomingByte == 'O') {  // indietro
+
+      myservo.write(90);  // stop
+      myservo.write(180);
+      delay(250);
+      myservo.write(90);
+    }
+
+    if (incomingByte == 'L') {  // avanti
+
+      myservo.write(90);  // stop
+      myservo.write(0);
+      delay(250);
+      myservo.write(90);
+    }
+    if (incomingByte == 'I') {  // avanti
+
+      myservo.write(90);  // stop
+      myservo.write(0);
+      delay(200);
+      myservo.write(180);
+      delay(200);
       myservo.write(90);
     }
   }
